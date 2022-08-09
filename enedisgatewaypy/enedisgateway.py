10844a93192b26915637aa2eb3aa621c6fa71477
cyr-ius <cyr-ius@ipocus.net>
2022-08-09 18:44:40 +0200

    fix list

--- enedisgatewaypy/enedisgateway.py
+++ enedisgatewaypy/enedisgateway.py
@@ -46,7 +46,7 @@ class EnedisByPDL(EnedisGateway):
     def __init__(self, pdl: str, token: str, session: ClientSession = None):
         """Initialize."""
         super().__init__(pdl, token)
-        self.offpeaks: list(str, str) = None
+        self.offpeaks: list(str) = None
 
     async def async_fetch_datas(self, service: str, start: datetime, end: datetime) -> ClientResponse:
         """Get datas."""
