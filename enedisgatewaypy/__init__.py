b25e17167eb0a50d42b21bb246998b04faf1761b
cyr-ius <cyr-ius@ipocus.net>
2022-10-23 16:52:12 +0200

    Update workflow

--- enedisgatewaypy/__init__.py
+++ enedisgatewaypy/__init__.py
@@ -1,9 +1,13 @@
 # -*- coding:utf-8 -*-
 
-"""Provides authentification and row access to Enedis Gateway website."""
+"""enedisgatewaypy package."""
 from .enedisgateway import EnedisGateway, EnedisByPDL
 from .exceptions import EnedisException, LimitReached, GatewayException
 
-name = "EnedisGateway"
-__version__ = "1.3"
-__all__ = ["EnedisGateway", "EnedisByPDL", "EnedisException", "LimitReached", "GatewayException"]
+__all__ = [
+    "EnedisGateway",
+    "EnedisByPDL",
+    "EnedisException",
+    "LimitReached",
+    "GatewayException"
+]
